e2a91bf63491b37f70bb91df4615df2c99a505b3
Kevin Tavares <kvatavares30@icloud.com>
2023-11-28 17:44:49 -0300

    about-100%

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,71 @@
+import { motion } from 'framer-motion';
 import React from 'react';
-
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+import imageperfil from '../assets/image.png';
+import { fadeIn } from '../variants';
 const About = () => {
-  return <div className='section' id='about'>
-    About
+  const [ref,inView] = useInView({
+    threshold: 0.5,
+  })
+  return (
+    <section className='section' id='about' ref={ref}>
+     <div className='container mx-auto'>
     
-   
- </div>;
+    <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20'>
+
+      {/* img*/ }
+      <motion.div
+      variants={fadeIn ('right',0.4)}
+      initial='hidden'
+      whileInView={'show'}
+      viewport={{once:false,amount:0.3}}
+      className='items-center' >
+       
+        <img className='flex-1 bg-contain bg-no-repeat h-[440px] mix-blend-lighten bg-top ' src={imageperfil} alt='about' />
+      </motion.div>
+      
+      {/* text*/ }
+      <motion.div 
+       variants={fadeIn ('left',0.2)}
+       initial='hidden'
+       whileInView={'show'}
+       viewport={{once:false,amount:0.3}}
+      
+      className='flex-1'>
+        <h2 className='h2 text-accent'>About me.</h2>
+        <h3 className='h3 mb-4'>
+          I'm a Software Engineer , specialist in front-end development certified by Meta (Facebook). 
+        </h3>
+        <p className='mb-6'>
+        I am currently a Full-Stack Engineer in a government agency in my country, <br/>
+         where we serve around 2 thousand monthly accesses.
+        </p>
+       {/* stats*/ }
+       <div className='flex '>
+           <div>
+            <div className='text-[40px] font-tertiary text-gradient mb-2'>
+              {
+                inView ?
+                <CountUp start={0} end={4} duration={5}/>  : null}
+            </div>
+
+            <div className='font-primary text-sm tracking-[2px]'>Projects Freelancer in<br/> 
+            Production 
+            </div>
+            <div className='flex gap-x-8 items-center ]'>
+              <button className='btn btn-lg mt-[20px]'>contact Me</button>
+              <a href='#' className='text-gradient btn-link mt-[14px]'>
+                My Portifolio
+              </a>
+            </div>
+           </div>
+       </div>
+      </motion.div>
+    </div>
+  </div>
+    </section>
+  )
 };
 
 export default About;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,47 +1,83 @@
+import { motion } from 'framer-motion';
 import React from 'react';
-import Image from '../assets/banner2.png';
-
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
+import Image from '../assets/moldura-perfil.png';
+import { fadeIn } from '../variants';
 const Banner = () => {
   return (
-  <section  className='section' id='home'>
-  <div className='container mx-auto'>
-    <div>
-
-    {/* text*/ }
-    <div> 
-      <h1>
-        Kevin <span>Dev</span>
-      </h1>
-    </div>
-    <div className='mb-6 text-[36px] lg:text-[60px] front-secondary front-semibold uppercase leading-[1]'>
-      <span className='text-white mr-2'>I am a</span>
-      {/* animations */}
-      <TypeAnimation sequence={[
-        'Front-End Engineer ',
-        2000,
-        'Mobile Engineer',
-        2000,
-        'FullStack Engineer',
-        2000
-      ]}
-      speed={50}
-      className='text-accent'
-      wrapper='span'
-      repeat={Infinity}
-      />
-   
-   </div>
-     {/* IMG */ }
-   
-     <div>
-    <img className='w-[250px] h-[300px]' src={Image}alt='foto-edit '/>
-     </div>
-    
-    </div>
-
-  </div>
   
+  <section  className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+        {/* text*/ }
+        
+        <div className='flex-1 text-center font-secondary lg:text-left'>
+        
+          < motion.h1 variants={fadeIn('up',0.3)} initial='hidden' 
+          whileInView={'show'} viewport={{once:false,amount:0.7}} 
+          className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>Kevin <span>DEV</span>
+          </motion.h1>
+        
+          <motion.div variants={fadeIn('up',0.6)} initial='hidden' 
+          whileInView={'show'} viewport={{once:false,amount:0.7}}  className='mb-6 text-[36px] lg;text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+            <span className='text-white mr-4'>I am a</span>
+        
+            <TypeAnimation sequence={[
+             'Front End Engineer',
+          2000,
+             'Mobile Engineer',
+          2000,
+             'Full-Stack Engineer',
+          2000,
+        ]}
+        speed={50}
+        className='text-accent'
+        wrapper='span'
+        repeat={Infinity}
+        />
+        
+          </motion.div>
+         
+          <motion.p variants={fadeIn('up',0.5)} initial='hidden' 
+          whileInView={'show'} viewport={{once:false,amount:0.7}} 
+           className='mb-8 max-w-lg mx-auto lg:mx-0'>
+          New Generation of Software Engineering.
+           </motion.p>
+           
+           <motion.div variants={fadeIn('up',0.7)} initial='hidden' 
+          whileInView={'show'} viewport={{once:false,amount:0.7}}  className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+             <button className='btn btn-lg'>Contact Me</button>
+             <a href='#' className='text-gradient btn-link'>My Portifolio</a>
+           </motion.div>
+        
+        {/* social m*/ }
+        <motion.div variants={fadeIn('up',0.8)} initial='hidden' 
+          whileInView={'show'} viewport={{once:false,amount:0.7}} 
+           className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+          
+         <a href='#'>
+            <FaGithub/>
+          </a>
+          
+          <a href='#'>
+            <FaInstagram/>
+          </a>
+          
+          <a href='#'>
+            <FaLinkedin/>
+          </a>
+        
+        </motion.div>
+
+        </div>
+           {/* image*/ }
+           <motion.div variants={fadeIn('down',0.5)} initial='hidden' 
+          whileInView={'show'}   className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] '>
+          <img src={Image} alt='perfil-color ' className='w-[270px] h-[400px]'/>
+           </motion.div>
+      </div>
+    </div>
   </section>
 )};
 
